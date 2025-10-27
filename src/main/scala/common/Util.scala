7debf5a9edef266e5fb3a62b24961b44f0b17e50
Albert ten Napel <aptennap@gmail.com>
2025-10-27 12:57:28 +0100

    Disable timing

--- src/main/scala/common/Util.scala
+++ src/main/scala/common/Util.scala
@@ -1,9 +1,13 @@
 package common
 
 object Util:
+  private inline val EnableTiming = false
+
   inline def time[A](label: String)(inline action: A): A =
-    val t = System.nanoTime()
-    val res = action
-    val t2 = System.nanoTime() - t
-    println(s"timed $label: ${t2}ns (${t2 / 1000000}ms)")
-    res
+    inline if EnableTiming then
+      val t = System.nanoTime()
+      val res = action
+      val t2 = System.nanoTime() - t
+      println(s"timed $label: ${t2}ns (${t2 / 1000000}ms)")
+      res
+    else action
